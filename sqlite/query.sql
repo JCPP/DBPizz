52481de7d11a39286e0034e5a44792f5dc21d99f
Davide Pastore <pasdavide@gmail.com>
2013-06-13 17:14:56 +0200

    Aggiunti i commenti per le query.

--- sqlite/query.sql
+++ sqlite/query.sql
@@ -1,31 +1,50 @@
+/**
+ * Creazione tabella Cliente
+ */
 CREATE TABLE Cliente IF NOT EXISTS(
 	IDCliente INTEGER(11) PRIMARY KEY,
 	Nome VARCHAR(50) NOT NULL,
 	Cognome VARCHAR(30) NOT NULL
 );
 
+/**
+ * Creazione tabella Prodotto.
+ */
 CREATE TABLE Prodotto IF NOT EXISTS(
 	IDProdotto INTEGER(11) PRIMARY KEY,
 	Nome VARCHAR(30) NOT NULL
 );
 
+/**
+ * Creazione tabella Ordine (associazione tra Cliente e Prodotto).
+ */
 CREATE TABLE Ordine IF NOT EXISTS(
 	IDCliente INTEGER(11),
 	IDProdotto INTEGER(11),
 	PRIMARY KEY(IDCliente, IDProdotto)
 );
 
+
+/**
+ * Creazione tabella Ingrediente.
+ */
 CREATE TABLE Ingrediente IF NOT EXISTS(
 	IDIngrediente INTEGER(11) PRIMARY KEY,
 	Nome VARCHAR(30) NOT NULL
 );
 
+/**
+ * Creazione tabella CompostoDa (associazione tra Prodotto e Ingrediente).
+ */
 CREATE TABLE CompostoDa IF NOT EXISTS(
 	IDProdotto INTEGER(11),
 	IDIngrendiente INTEGER(11),
 	PRIMARY KEY(IDProdotto, IDIngrendiente)
 );
 
+/**
+ * Creazione tabella Pizzeria.
+ */
 CREATE TABLE Pizzeria IF NOT EXISTS(
 	IDPizzeria INTEGER(11) PRIMARY KEY,
 	Nome VARCHAR(40) NOT NULL,
@@ -37,6 +56,9 @@ CREATE TABLE Pizzeria IF NOT EXISTS(
 	Asporto BOOLEAN NOT NULL
 );
 
+/**
+ * Creazione tabella Appartiene (associazione tra Prodotto e Pizzeria).
+ */
 CREATE TABLE Appartiene IF NOT EXISTS(
 	IDProdotto INTEGER(11),
 	IDPizzeria INTEGER(11),
