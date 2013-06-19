5a1779e2aaf1457bc18808de9b578e7cf53f7162
Matteo Calò <emmeci92@gmail.com>
2013-06-19 16:19:51 +0200

    Eliminati i todo e aggiunti gli issues per la gestione dei miglioramenti.

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 /**
  * Si registra l'autoloader per i componenti installati.
- * @todo inserire la data dell'ordine.
- * @todo possibilità di vedere tutti gli ordini di un determinato giorno.
- * @todo cerca pizzeria versatile.
- * @todo incasso giornaliero di una pizzeria tramite portale.
- * @todo cerca prodotto in base agli ingredienti.
+ *
  */
 require 'vendor/autoload.php';
 
