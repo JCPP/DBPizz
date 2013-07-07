7e4d01664483557500b2f233aa7755860242b42c
DavidePastore <pasdavide@gmail.com>
2013-07-07 17:45:46 +0200

    Closed #35.

--- app/api/apiProdotto.php
+++ app/api/apiProdotto.php
@@ -1,10 +1,8 @@
 <?php
-/**
- * @todo inserire gli ingredienti con la chiamata alle API.
- */
 //Creare un nuovo prodotto
 $app->post('/api/prodotti/add', function () use($app){
-	$json = $app->request()->getBody();
+	$request = $app->request();
+	$json = $request->getBody();
 	$prodotto = json_decode($json);
 	
 	array_walk_recursive($prodotto, function(&$value, $key){
@@ -27,12 +25,35 @@ $app->post('/api/prodotti/add', function () use($app){
 	$prodottoDB->save();
 	
 	if(property_exists($prodotto, "ingredienti")){
+		$url = $request->getUrl() . $app->urlFor("APICompostodaAdd");
+		
 		//Aggiungo gli ingredienti al prodotto
-		foreach($prodotto->ingredienti as $compostoda){
+		foreach($prodotto->ingredienti as $ingrediente){
+			
+			$postVars = array(
+				"idProdotto" => $prodottoDB->id(),
+				"idIngrediente" => $ingrediente
+			);
+			
+			$response = \Httpful\Request::post($url)    // Build a POST request...
+			->sendsJson()                               // tell it we're sending (Content-Type) JSON...
+			->body(json_encode($postVars))             	// attach a body/payload...
+			->send();                                   // and finally, fire that thing off!
+			
+			$body = $response->body;
+			
+			$risposta = json_decode($body);
+			
+			if(property_exists($risposta, "errore")){
+				exit('{"errore": "campo ' . $key . ' non inserito"}');
+			}
+			
+			/*
 			$compostodaDB = Model::factory('CompostoDa')->create();
 			$compostodaDB->IDProdotto = $prodottoDB->id();
-			$compostodaDB->IDIngrediente = $compostoda;
+			$compostodaDB->IDIngrediente = $ingrediente;
 			$compostodaDB->save();
+			*/
 		}
 	}
 	
